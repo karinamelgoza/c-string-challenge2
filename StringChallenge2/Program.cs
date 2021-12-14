bb4e464cb74f01af072368610651676694abf473
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-14 12:02:04 -0700

    Adds practice with concat method

--- StringChallenge2/Program.cs
+++ StringChallenge2/Program.cs
@@ -9,15 +9,16 @@ namespace StringChallenge2
             Console.Write("Enter a string here: ");
             string userInput = Console.ReadLine();
             Console.Write("Enter the character to search: ");
-            string characterToSearch = Console.ReadLine();
-            Console.WriteLine(userInput.IndexOf(characterToSearch));
+            char characterToSearch = Console.ReadLine()[0];
+            int indexOfCharacter = userInput.IndexOf(characterToSearch);
+            Console.WriteLine($"Index of character {characterToSearch} is {indexOfCharacter}");
 
             Console.Write("Enter first name: ");
             string firstName = Console.ReadLine();
             Console.Write("Enter last name: ");
             string lastName = Console.ReadLine();
-            string fullName = string.Format($"{firstName} {lastName}");
-            Console.WriteLine(fullName);
+            string fullName = string.Concat(firstName, " ", lastName);
+            Console.WriteLine($"Your full name is: {fullName}");
 
         }
     }
