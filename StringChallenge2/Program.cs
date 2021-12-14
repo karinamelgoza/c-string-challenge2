4bca97bac41e53f4661f6ec44e0c24fa3ab24369
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-14 11:48:26 -0700

    Adds string methods practice

--- StringChallenge2/Program.cs
+++ StringChallenge2/Program.cs
@@ -6,7 +6,19 @@ namespace StringChallenge2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a string here: ");
+            string userInput = Console.ReadLine();
+            Console.Write("Enter the character to search: ");
+            string characterToSearch = Console.ReadLine();
+            Console.WriteLine(userInput.IndexOf(characterToSearch));
+
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+            string fullName = string.Format($"{firstName} {lastName}");
+            Console.WriteLine(fullName);
+
         }
     }
 }
